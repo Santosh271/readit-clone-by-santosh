99dd16238ea387e50d97385fafc535120a4e7a57
Santosh271 <97075337+Santosh271@users.noreply.github.com>
2022-03-11 12:30:40 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function upvote(){
+    let votes = document.getElementById("votes");
+    let count = parseInt(votes.innerHTML);
+    count++;
+    votes.innerHTML = count;
+}
+
+function downvote(){
+    let votes = document.getElementById("votes");
+    let count = parseInt(votes.innerHTML);
+    count--;
+    votes.innerHTML = count;
+}
